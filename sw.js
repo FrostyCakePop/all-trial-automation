a3ee8c7bdc97d4d4ad97826ce01cacb34e2cc510
FrostyCakePop <vincentpsison@gmail.com>
2025-06-18 18:02:09 -0700

    Update sw.js
    pwa support files

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
-const CACHE = 'warmer-v1';
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])))
-);
-self.addEventListener('fetch', e =>
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
-);
+const CACHE_NAME = 'email-warmer-v1';
+const urlsToCache = [
+  '/',
+  'manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
