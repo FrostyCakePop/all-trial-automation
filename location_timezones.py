afc730a6b27ac53457195c84ef5dad8773c08d4b
FrostyCakePop <vincentpsison@gmail.com>
2025-06-20 02:35:56 -0700

    Create location_timezones.py

---
+++ location_timezones.py
@@ -0,0 +1,8 @@
+location_timezones = {
+    "Riverside, CA": "America/Los_Angeles",
+    "La Jolla, CA": "America/Los_Angeles",
+    "Los Angeles, CA": "America/Los_Angeles",
+    "Diamond Bar, CA": "America/Los_Angeles",
+    "Orange, CA": "America/Los_Angeles",
+    "San Bernardino, CA": "America/Los_Angeles"
+}
